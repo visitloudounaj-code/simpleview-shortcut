96e9d0eb7e0bbb9ef8c2e1a9dd9600ed50c2d8ee
visitloudounaj-code <visitloudoun.aj@gmail.com>
2026-02-23 11:59:15 -0500

    Update background.js
    Added another automation that opens the window to select the link type.

--- background.js
+++ background.js
@@ -4,8 +4,23 @@ chrome.commands.onCommand.addListener((command) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         func: () => {
+          // Step 1: Click the link button to open the dialog
           const linkBtn = document.querySelector('.cke_button__linklookup_button');
-          if (linkBtn) linkBtn.click();
+          if (linkBtn) {
+            linkBtn.click();
+
+            // Step 2: Wait for the dialog to appear, then click Browse
+            const interval = setInterval(() => {
+              const browseBtn = document.querySelector('a[title="Browse"].cke_dialog_ui_button');
+              if (browseBtn) {
+                clearInterval(interval);
+                browseBtn.click();
+              }
+            }, 100);
+
+            // Stop trying after 5 seconds if dialog never appears
+            setTimeout(() => clearInterval(interval), 5000);
+          }
         }
       });
     });
